bdacd88f78911e4b4f343f8ebeb81a71037f8a68
HeshamFawzy <46792902+HeshamFawzy@users.noreply.github.com>
2021-02-19 20:21:33 +0200

    project done

--- app/src/main/java/com/hwave/findmyage/MainActivity.kt
+++ app/src/main/java/com/hwave/findmyage/MainActivity.kt
@@ -2,10 +2,20 @@ package com.hwave.findmyage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun buClickEvent(view: View) {
+        val yearOfBirth = etUserYearOfBirth.text.toString().toInt()
+        val currentYear = Calendar.getInstance().get(Calendar.YEAR)
+        val age = currentYear - yearOfBirth
+        tvShowAge.text = "Your Age is " + age + " Years"
     }
 }
